e361540d1d547f3a6aa307465388cf0d93fc867b
stanleyyoga123 <yoga360227@gmail.com>
2020-10-01 18:21:51 +0700

    Fix move to base and add documentation

--- src/model/board.py
+++ src/model/board.py
@@ -1,6 +1,8 @@
 from .pawn import Pawn
 from .tile import Tile
-from .color import Color    
+from .color import Color
+
+from colorama import Fore, Style
 
 class Board():
     def __init__(self, b_size, pawns, tiles):
@@ -30,86 +32,118 @@ class Board():
                         out += pawn.__str__()
                 if not is_pawn:
                     out += '.'
+                out += ' '
                 is_pawn = False
             out += '\n'
         return out
 
     def get_pawn(self, location):
-        '''
-        location = tuple(int, int)
+        '''Find pawn based on their location in board
+
+        Parameters:
+            location (Tuple(int, int)): Location Pawn
+
+        Returns:
+            Pawn: Pawn Founds
         '''
         for pawn in self.pawns:
             if pawn.position.location == location:
                 return pawn
         return None
 
-    def check_tile_condition(self, before, after):
+    def base_field_checker(self, pawn, after):
+        '''Check if pawn move back to its base, or 
+        move out from enemy base
+
+        Parameters:
+            pawn (Pawn): Selected Pawn 
+            after (Tile): Destination Tile
+
+        Returns:
+            boolean: True if they move back to its base or move out from enemy base  
         '''
-        before: Pawn
-        after: Pawn
-        '''
-        return ((before.has_red and before.has_neutral and after.has_red and not after.has_neutral) or
-                (before.has_green and before.has_neutral and after.has_red and not after.has_neutral) or
-                (before.has_red and not before.has_neutral and after.has_green and not after.has_neutral) or
-                (before.has_green and not before.has_neutral and after.has_red and not after.has_neutral))
+        neutral_to_base = (pawn.has_red and pawn.has_neutral and after.color == Color.RED) or \
+                            (pawn.has_green and pawn.has_neutral and after.color == Color.GREEN)
+        base_to_neutral = pawn.has_red and pawn.has_green and after.color == Color.NEUTRAL
+        return neutral_to_base or base_to_neutral
     
-    def is_out_board(self, location):
+    def is_out_board(self, destination):
+        '''Check if destinations is out of board
+
+        Parameters:
+            destination (Tuple(int, int)): Destination Tiles 
+
+        Returns:
+            boolean: True if destination is out of board
         '''
-        location: tuple(int, int)
-        '''
-        return (location[0] >= self.b_size or location[1] >= self.b_size or location[0] < 0 or location[1] < 0)
+        return (destination[0] >= self.b_size or destination[1] >= self.b_size or destination[0] < 0 or destination[1] < 0)
         
-    def check_move(self, pawn, direction):
-        '''
-        pawn: Pawn
-        direction: String
+    def check_jump(self, pawn, direction):
+        '''Check if pawn can jump in which direction
+
+        Parameters:
+            pawn (Pawn): Selected Pawn
+            direction (String): Direction which pawn will jump
+        
+        Returns:
+            boolean: True if pawn can jump
+            Pawn: Temporary Pawn with location after jumping
         '''
         can_jump = False
-        can_move = False
         updated_pawn_location = (pawn.position.location[0] + self.list_direction[direction][0],
                                 pawn.position.location[1] + self.list_direction[direction][1])
 
         if not self.is_out_board(updated_pawn_location):
-            if self.get_pawn(updated_pawn_location) is None:
-                can_move = True
-            else:
+            if self.get_pawn(updated_pawn_location) is not None:
                 updated_pawn_location = (updated_pawn_location[0] + self.list_direction[direction][0], 
                                         updated_pawn_location[1] + self.list_direction[direction][1])
-                if self.get_pawn(updated_pawn_location) is None and not self.is_out_board(updated_pawn_location):
-                    can_move = True
+                if (self.get_pawn(updated_pawn_location) is None) and \
+                        (not self.is_out_board(updated_pawn_location)):
                     can_jump = True
                     temp_pawn = pawn.temp_copy(position=self.tiles[updated_pawn_location[0]][updated_pawn_location[1]])
                     temp_pawn = self.change_color_pawn(temp_pawn, self.tiles[updated_pawn_location[0]][updated_pawn_location[1]])
 
         if can_jump:
-            return (can_move, can_jump, temp_pawn)
+            return (can_jump, temp_pawn)
         else:
-            return (can_move, can_jump, None)
+            return (can_jump, None)
 
     def possible_jump(self, pawn, history):
-        '''
-        pawn: Pawn
-        history: Array
+        '''Get all possibility jump from selected pawn
+
+        Parameters:
+            pawn (Pawn): Selected Pawn
+            history (Array(Pawn)): History jump from selected Pawn
+        
+        Returns:
+            Array(Pawn): List of possibility jump from selected Pawn
         '''
         for key, _ in self.list_direction.items():
-            _, can_jump, pawn_after = self.check_move(pawn, key)
+            can_jump, pawn_after = self.check_move(pawn, key)
             if can_jump and pawn_after not in history:
-                if self.check_tile_condition(pawn, pawn_after):
+                if self.base_field_checker(pawn, pawn_after.position):
                     continue
                 history.append(pawn_after)
                 history = self.possible_jump(pawn_after, history)
         return history  
     
     def adjacent_move(self, pawn):
-        '''
-        pawn: Pawn
+        '''Get all possibiliy adjacent moves from selected Pawn
+
+        Parameters:
+            pawn (Pawn): Selected Pawn
+
+        Returns:
+            Array(Pawn): List of possibiliy adjacent moves from selected Pawn
         '''
         possible_move = []
         for _, val in self.list_direction.items():
             updated_pawn_location = (pawn.position.location[0] + val[0],
                                     pawn.position.location[1] + val[1])
-            
+
             if self.get_pawn(updated_pawn_location) is None and not self.is_out_board(updated_pawn_location):
+                if (self.base_field_checker(pawn, self.tiles[updated_pawn_location[0]][updated_pawn_location[1]])):
+                    continue
                 temp_pawn = pawn.temp_copy(position=self.tiles[updated_pawn_location[0]][updated_pawn_location[1]])
                 temp_pawn = self.change_color_pawn(temp_pawn, self.tiles[updated_pawn_location[0]][updated_pawn_location[1]])
                 possible_move.append(temp_pawn)
@@ -117,9 +151,14 @@ class Board():
         return possible_move
     
     def change_color_pawn(self, pawn, tile):
-        '''
-        pawn: Pawn
-        tile: Tile
+        '''Change history of color tiles passed from pawn
+
+        Parameters:
+            pawn (Pawn): Selected Pawn
+            tile (Tile): Tile where selected Pawn landed after move
+
+        Returns:
+            Pawn: Updated Pawn 
         '''
         if tile.color == Color.RED:
             pawn.has_red = True
@@ -130,16 +169,24 @@ class Board():
         return pawn
 
     def possible_moves(self, pawn):
-        '''
-        pawn: Pawn
+        '''Get all possible moves from selected Pawn
+        
+        Parameters:
+            pawn (Pawn): Selected Pawn
+        
+        Returns:
+            Array(Pawn): List of possible moves from selected Pawn
         '''
         adjacent_move = self.adjacent_move(pawn)
         jump_move = self.possible_jump(pawn, [])
         return adjacent_move + jump_move
 
     def move_pawn(self, before, after):
-        '''
-        before, after: Pawn
+        '''Update Pawn to new location
+
+        Parameters:
+            before (Pawn): Selected Pawn
+            after (Pawn): Destination Pawn
         '''
         print(before.position)
         print(after.position)
--- src/model/pawn.py
+++ src/model/pawn.py
@@ -5,8 +5,14 @@ class Pawn():
         self.id = id
         self.position = position
         self.color = color
-        self.has_red = False
-        self.has_green = False
+
+        if color == Color.RED:
+            self.has_red = True
+            self.has_green = False
+        else:
+            self.has_green = True
+            self.has_red = False
+
         self.has_neutral = False
     
     def __str__(self):
@@ -19,14 +25,27 @@ class Pawn():
         return self.position.location == pawn.position.location
 
     def temp_copy(self, position):
+        '''Create new pawn from current pawn with updated position
+
+        Parameters:
+            position (Tile): New Position
+        
+        Returns:
+            Pawn: New Pawn with updated position
+        '''
         temp_pawn = Pawn(self.id, position, self.color)
         temp_pawn.has_red = self.has_red
-        temp_pawn.has_green = self.has_red
+        temp_pawn.has_green = self.has_green
         temp_pawn.has_neutral = self.has_neutral
 
         return temp_pawn
     
     def copy(self, pawn):
+        '''Copy Pawn
+        
+        Parameters:
+            pawn (Pawn): Destination Pawn
+        '''
         self.position = pawn.position
         self.has_red = pawn.has_red
         self.has_green = pawn.has_green
